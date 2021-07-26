be97f24acfff3667613ffe122f01b8396de1eb19
blais <blais@furius.ca>
2021-07-26 00:34:22 -0400

    Added script for switching the definition of transaction ids. I'm doing this to eventually get rid of order ids as a way to define finalized chains.

---
+++ experiments/translate-transaction-ids.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+"""Translate transaction ids in the input file.
+
+This is used temporarily after changing the definition of the transaction ids to
+leverage similarity in the order ids.
+"""
+__copyright__ = "Copyright (C) 2021  Martin Blais"
+__license__ = "GNU GPLv2"
+
+import logging
+import os
+from typing import List, Optional
+
+import click
+
+from johnny.base import discovery
+from johnny.base import mark
+from johnny.base import chains as chainslib
+from johnny.base import config as configlib
+from johnny.base import instrument
+from johnny.base.etl import petl, Table
+
+
+@click.command()
+@click.option('--config', '-c',
+              help="Configuration filename. Default to $JOHNNY_CONFIG")
+def main(config: Optional[str]):
+    "Find, process and print transactions."
+
+    filename = configlib.GetConfigFilenameWithDefaults(config)
+    config = configlib.ParseFile(filename)
+    logtables = discovery.ReadConfiguredInputs(config)
+    table = (logtables[configlib.Account.LogType.TRANSACTIONS]
+             .selectne('transaction_new_id', None))
+
+    new_ids = list((table
+                    .values('transaction_new_id')))
+    assert len(list(new_ids)) == len(set(new_ids)), (
+        len(list(new_ids)), len(set(new_ids)))
+
+    mapping = (table
+               .lookup('transaction_id', 'transaction_new_id'))
+
+    for chain in config.chains:
+        if not chain.transaction_ids:
+            continue
+        ids = list(chain.transaction_ids)
+        chain.ClearField('transaction_ids')
+        for id in ids:
+            try:
+                mapped = mapping[id]
+                for newid in mapped:
+                    if newid is None:
+                        continue
+                    chain.transaction_ids.append(newid)
+            except KeyError:
+                chain.transaction_ids.append(id)
+
+    print(config)
+    #pp(mapping)
+
+    #print(table.lookallstr())
+
+
+if __name__ == '__main__':
+    main(obj={})
