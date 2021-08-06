dc2f8e4a2215c60109ad03c3bc3cb7bdf635ccd4
blais <blais@furius.ca>
2021-08-06 13:10:31 -0400

    Fixed bug.

--- experiments/accept-all.py
+++ experiments/accept-all.py
@@ -20,11 +20,11 @@ def main(config: Optional[str]):
     filename = configlib.GetConfigFilenameWithDefaults(config)
     config = configlib.ParseFile(filename)
     chains_db = configlib.ReadChains(config.input.chains_db)
-    chain_map = {c.chain_id: c for c in chains_dbconfig.chains}
+    chain_map = {c.chain_id: c for c in chains_db.chains}
     for chain in chain_map.values():
         if chain.status == ChainStatus.CLOSED:
             chainslib.AcceptChain(chain, status=ChainStatus.FINAL)
-    print(configlib.ToText(config))
+    print(configlib.ToText(chains_db))
 
 
 if __name__ == '__main__':
